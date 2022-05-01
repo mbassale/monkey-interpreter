06b85a204ca8257935590723e39c54dfe0c90ea5
Marco Bassaletti <bassaletti@gmail.com>
2022-05-01 14:18:16 -0700

    [MonkeyLang] Initial lexer implementation

---
+++ token/token.go
@@ -0,0 +1,39 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func NewToken(tokenType TokenType, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers and literals
+	IDENTIFIER = "IDENTIFIER"
+	INT        = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
