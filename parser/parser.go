960131a71f17a45a3019e1fd9cf533282d6d3c01
Marco Bassaletti <bassaletti@gmail.com>
2022-07-07 18:02:47 -0700

    [MonkeyLang] Partial LetStatement Parsing

--- parser/parser.go
+++ parser/parser.go
@@ -22,11 +22,66 @@ func New(lexer *lexer.Lexer) *Parser {
 	return parser
 }
 
+func (parser *Parser) ParseProgram() *ast.Program {
+	program := ast.NewProgram()
+	for parser.currentToken.Type != token.EOF {
+		stmt := parser.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		parser.nextToken()
+	}
+	return program
+}
+
 func (parser *Parser) nextToken() {
 	parser.currentToken = parser.peekToken
 	parser.peekToken = parser.lexer.NextToken()
 }
 
-func (parser *Parser) ParseProgram() *ast.Program {
-	return nil
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currentToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+
+	stmt := &ast.LetStatement{Token: parser.currentToken}
+
+	if !parser.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Literal}
+
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: add expression parsing
+	for !parser.currentTokenIs(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	return stmt
+}
+
+func (parser *Parser) currentTokenIs(tokenType token.TokenType) bool {
+	return parser.currentToken.Type == tokenType
+}
+
+func (parser *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return parser.peekToken.Type == tokenType
+}
+
+func (parser *Parser) expectPeek(tokenType token.TokenType) bool {
+	if parser.peekTokenIs(tokenType) {
+		parser.nextToken()
+		return true
+	}
+	return false
 }
