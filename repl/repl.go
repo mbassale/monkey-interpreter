aab07d633be01bc90f28c1d918400b885547f728
Marco Bassaletti <bassaletti@gmail.com>
2022-07-11 21:04:43 -0700

    [MonkeyLang][Repl] Adding parsing

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
+	"monkey/parser"
 	"monkey/token"
 )
 
@@ -22,9 +23,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Fprintf(out, "%+v\n", tok)
 		}
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
