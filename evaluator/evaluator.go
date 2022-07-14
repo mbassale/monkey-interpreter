f9a650eb9559b29865d008528cc6da9ea0e83f74
Marco Bassaletti <bassaletti@gmail.com>
2022-07-13 21:42:13 -0700

    [MonkeyLang][Evaluator] Minus Integer Expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -44,6 +44,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -60,6 +62,15 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
